898ea52f5973a709ba4897e76d76b02d5fda7c68
iamhosto10 <gerav0400@gmail.com>
2022-10-27 09:09:10 -0500

    country added

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -19,12 +19,31 @@ function Form() {
     { value: "sports", label: "Sports" },
     { value: "technology", label: "Technology" },
   ];
+  const Paises = [
+    { value: "co", label: "Colombia" },
+    { value: "mx", label: "Mexico" },
+  ];
   return (
     <form>
       <FormControl fullWidth>
         <InputLabel>Categorías</InputLabel>
         <Select value={categoria} onChange={handlechange} label="Categoría">
           {CATEGORÍAS.map((categorias) => (
+            <MenuItem key={categorias.value} value={categorias.value}>
+              {categorias.label}
+            </MenuItem>
+          ))}
+        </Select>
+        <Box sx={{ marginTop: 2 }}>
+          <Button fullWidth variant="contained">
+            Search Caregory
+          </Button>
+        </Box>
+      </FormControl>
+      <FormControl fullWidth sx={{ marginTop: 2 }}>
+        <InputLabel>Paises</InputLabel>
+        <Select value={"co"} onChange={handlechange} label="Categoría">
+          {Paises.map((categorias) => (
             <MenuItem key={categorias.value} value={categorias.value}>
               {categorias.label}
             </MenuItem>
--- src/Contexts/Newsprovider.js
+++ src/Contexts/Newsprovider.js
@@ -14,14 +14,21 @@ export const Newsprovider = ({ children }) => {
   useEffect(() => {
     const restAPI = async () => {
       console.log("entro a la api");
-      // const apikey = process.env.REACT_APP_API_KEY;
+      const apikey = process.env.REACT_APP_API_KEY;
+      console.log(apikey, "apikey");
       // const url = `https://newsapi.org/v2/top-headlines?country=co&categoria=${categoria}&apiKey=
       //  ${import.meta.env.API_KEY}
       // `;
-      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=392e19d2ce964e69b37a9656cb015ecf`;
-      const { data } = await axios(url);
-      console.log(process.env.REACT_APP_API_KEY);
-      setNews(data.articles);
+      // const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=392e19d2ce964e69b37a9656cb015ecf`;
+      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=${apikey}`;
+      console.log(url, "apikey");
+      try {
+        const { data } = await axios.get(url);
+        console.log(process.env.REACT_APP_API_KEY, "apikey");
+        setNews(data.articles);
+      } catch (e) {
+        console.log(e);
+      }
     };
     restAPI();
   }, [categoria]);
